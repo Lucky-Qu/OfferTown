f7630c8a8f79b56f30af253be71e795196cee9a1
LuckyQu <super.lucky.qu@gmail.com>
2025-09-24 16:48:56 +0800

    fix: 使用包装状态码而非数字

--- backend/internal/api/ping.go
+++ backend/internal/api/ping.go
@@ -1,19 +1,23 @@
-//Package api ping.go
+// Package api ping.go
 //
-//功能：
-//- 处理Ping请求
+// 功能:
+// - 处理Ping请求
 //
-//作者: LuckyQu
-//日期: 2025-09-24
+// 作者: LuckyQu
+// 创建日期: 2025-09-24
+// 修改日期: 2025-09-24
 
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"backend/internal/code"
+	"github.com/gin-gonic/gin"
+)
 
-// PingHandler 返回正常代码200并返回json格式的msg内容为pong
+// PingHandler 返回正常代码并返回json格式的msg内容为pong
 func PingHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
+		ctx.JSON(code.HttpStatusOK, gin.H{
 			"message": "pong",
 		})
 	}
