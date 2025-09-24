c93baf501b7fb6ee5e8be6f43ec4947ef3b928ae
LuckyQu <super.lucky.qu@gmail.com>
2025-09-24 16:47:32 +0800

    feat: 包装http状态码

---
+++ backend/internal/code/httpStatus.go
@@ -0,0 +1,16 @@
+// Package code httpStatus.go
+//
+// 功能:
+// - 封装http状态码
+//
+// 作者: LuckyQu
+// 创建日期: 2025-09-24
+// 修改日期: 2025-09-24
+
+package code
+
+// Http状态码
+const (
+	HttpStatusOK         = 200
+	HttpStatusBadRequest = 400
+)
