e83a5c2cd760ce9e70abb83057135078909b43d4
LuckyQu <super.lucky.qu@gmail.com>
2025-09-25 17:48:58 +0800

    feat: 添加用户信息DTO

--- backend/internal/dto/user_dto.go
+++ backend/internal/dto/user_dto.go
@@ -5,7 +5,7 @@
 //
 // 作者: LuckyQu
 // 创建日期: 2025-09-24
-// 修改日期: 2025-09-24
+// 修改日期: 2025-09-25
 
 package dto
 
@@ -13,4 +13,15 @@ package dto
 type UserCreateDTO struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
+}
+
+type UserLoginDTO struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type UserInfoDTO struct {
+	Username   string `json:"username" binding:"required"`
+	Signature  string `json:"signature" binding:"required"`
+	AvatarPath string `json:"avatar_path" binding:"required"`
 }
