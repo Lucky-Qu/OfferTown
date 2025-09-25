b71554b063ca29c72ea0eaf30ffed8df3c7f0351
LuckyQu <super.lucky.qu@gmail.com>
2025-09-25 17:47:48 +0800

    feat: 用户登录，获取用户信息

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -7,11 +7,12 @@ package router
 //
 // 作者: LuckyQu
 // 创建日期: 2025-09-24
-// 修改日期: 2025-09-24
+// 修改日期: 2025-09-25
 
 import (
 	"backend/configs"
 	"backend/internal/api"
+	"backend/internal/middleware"
 	"github.com/gin-gonic/gin"
 	"strconv"
 )
@@ -27,6 +28,13 @@ func newRouter() *gin.Engine {
 		user := router.Group("/user")
 		{
 			user.POST("/register", api.UserRegisterHandler())
+			user.POST("/login", api.UserLoginHandler())
+			// 需要在登陆状态下进行的操作
+			loggedUser := user.Group("/")
+			loggedUser.Use(middleware.JWTAuth)
+			{
+				loggedUser.GET("/info", api.UserInfoHandler())
+			}
 		}
 	}
 
