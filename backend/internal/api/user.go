57990aa9e8ab6aa1536ca95da045409114ae6ce2
LuckyQu <super.lucky.qu@gmail.com>
2025-09-25 17:48:07 +0800

    feat: 用户登录，获取用户信息

--- backend/internal/api/user.go
+++ backend/internal/api/user.go
@@ -10,6 +10,7 @@
 package api
 
 import (
+	"backend/internal/auth"
 	"backend/internal/code"
 	"backend/internal/dto"
 	"backend/internal/service"
@@ -21,16 +22,73 @@ func UserRegisterHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		user := dto.UserCreateDTO{}
 		if err := ctx.ShouldBindJSON(&user); err != nil {
-			ctx.JSON(code.HttpStatusBadRequest, gin.H{
+			ctx.JSON(code.HttpStatusOK, gin.H{
 				"code":    code.BindFailed,
 				"message": code.BindFailed.Msg(),
 			})
 			return
 		}
 		eCode := service.RegisterUser(&user)
-		ctx.JSON(200, gin.H{
+		ctx.JSON(code.HttpStatusOK, gin.H{
 			"code":    eCode,
 			"message": eCode.Msg(),
+		})
+		return
+	}
+}
+
+// UserLoginHandler 处理用户登录请求
+func UserLoginHandler() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		user := dto.UserLoginDTO{}
+		if err := ctx.ShouldBindJSON(&user); err != nil {
+			ctx.JSON(code.HttpStatusOK, gin.H{
+				"code":    code.BindFailed,
+				"message": code.BindFailed.Msg(),
+			})
+			return
+		}
+		token, eCode := service.UserLogin(&user)
+		if eCode != code.Success {
+			ctx.JSON(code.HttpStatusOK, gin.H{
+				"code":    eCode,
+				"message": eCode.Msg(),
+			})
+			return
+		}
+		ctx.JSON(code.HttpStatusOK, gin.H{
+			"code":    eCode,
+			"message": eCode.Msg(),
+			"token":   token,
+		})
+		return
+	}
+}
+
+// UserInfoHandler 处理获取用户信息请求
+func UserInfoHandler() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		claims, exists := ctx.Get("claims")
+		if !exists {
+			ctx.JSON(code.HttpStatusOK, gin.H{
+				"code":    code.UnLoginUser,
+				"message": code.UnLoginUser.Msg(),
+			})
+			return
+		}
+		username := claims.(*auth.Claims).Username
+		user, eCode := service.GetUserInfoByUsername(username)
+		if eCode != code.Success {
+			ctx.JSON(code.HttpStatusOK, gin.H{
+				"code":    eCode,
+				"message": eCode.Msg(),
+			})
+			return
+		}
+
+		ctx.JSON(code.HttpStatusOK, gin.H{
+			"code": code.HttpStatusOK,
+			"data": user,
 		})
 	}
 }
