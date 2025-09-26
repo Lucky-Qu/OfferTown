c49e743ada32f685c74ddb0e6043556b28c4a9c7
LuckyQu <super.lucky.qu@gmail.com>
2025-09-26 15:52:33 +0800

    feat: 更新用户信息

--- backend/internal/service/user.go
+++ backend/internal/service/user.go
@@ -18,6 +18,7 @@ import (
 	"backend/internal/model"
 	"backend/internal/repository"
 	"backend/internal/validator"
+	"strconv"
 )
 
 // RegisterUser 注册用户功能
@@ -75,19 +76,19 @@ func UserLogin(userDTO *dto.UserLoginDTO) (string, code.Code) {
 		return "", code.PasswordWrong
 	}
 	// 检查是否已在登陆状态，避免重复生成Token
-	check, err := cache.CheckJWTIsExists(user.Username)
+	check, err := cache.CheckJWTIsExists(strconv.Itoa(int(user.ID)))
 	if err != nil {
 		return "", code.CacheError
 	}
 	if check {
-		token, err := cache.GetJWTByUsername(user.Username)
+		token, err := cache.GetJWTByUserid(strconv.Itoa(int(user.ID)))
 		if err != nil {
 			return "", code.CacheError
 		}
 		return token, code.Success
 	}
 
-	token, eCode := auth.GetToken(int(user.ID), user.Username)
+	token, eCode := auth.GetToken(int(user.ID))
 	if eCode != code.Success {
 		return "", eCode
 	}
@@ -96,9 +97,9 @@ func UserLogin(userDTO *dto.UserLoginDTO) (string, code.Code) {
 	return token, code.Success
 }
 
-// GetUserInfoByUsername 通过用户名获得用户DTO对象
-func GetUserInfoByUsername(username string) (*dto.UserInfoDTO, code.Code) {
-	user, err := repository.GetUserByUsername(username)
+// GetUserInfoByUserid 通过用户id获得用户DTO对象
+func GetUserInfoByUserid(userid string) (*dto.UserInfoDTO, code.Code) {
+	user, err := repository.GetUserByUserid(userid)
 	if err != nil {
 		return nil, code.DatabaseError
 	}
@@ -108,4 +109,72 @@ func GetUserInfoByUsername(username string) (*dto.UserInfoDTO, code.Code) {
 		AvatarPath: user.AvatarPath,
 	}
 	return &userDTO, code.Success
+}
+
+// UserUpdate 更新用户信息
+func UserUpdate(userDTO *dto.UserUpdateDTO, userIdStr string) code.Code {
+	//处理传入的userId
+	userIdInt, err := strconv.Atoi(userIdStr)
+	if err != nil {
+		return code.ServerError
+	}
+	userId := uint(userIdInt)
+	//取出要修改的用户信息
+	user, err := repository.GetUserByUserid(userIdStr)
+	if err != nil {
+		return code.DatabaseError
+	}
+	//构建map进行更新
+	updates := make(map[string]interface{})
+	//筛选出要更新的内容并加入到map
+	//-用户名
+	if userDTO.Username != "" && userDTO.Username != user.Username {
+		if !validator.UsernameCheck(userDTO.Username) {
+			return code.InvalidUsername
+		}
+		if validator.UsernameExistCheck(userDTO.Username) {
+			return code.UsernameAlreadyExist
+		}
+		updates["username"] = userDTO.Username
+	}
+	//-密码
+	if userDTO.Password != "" {
+		if !validator.PasswordCheck(userDTO.Password) {
+			return code.PasswordWrong
+		}
+
+		result, eCode := crypto.Verify(user.EncryptedPassword, userDTO.Password)
+		if eCode != code.Success {
+			return eCode
+		}
+		if !result {
+			encryptedPassword, eCode := crypto.Encrypt(userDTO.Password)
+			if eCode != code.Success {
+				return eCode
+			}
+			updates["encrypted_password"] = encryptedPassword
+			//如果修改密码，需要重新登录
+			err := cache.DeleteTokenByUserid(strconv.Itoa(int(user.ID)))
+			if err != nil {
+				return code.CacheError
+			}
+		}
+	}
+	//-个性签名
+	if userDTO.Signature != user.Signature {
+		if !validator.SignatureCheck(userDTO.Signature) {
+			return code.OverlongSignature
+		}
+		updates["signature"] = userDTO.Signature
+	}
+
+	if len(updates) == 0 {
+		return code.Success
+	}
+	//传入数据层操作
+	err = repository.UpdateUser(updates, uint(userId))
+	if err != nil {
+		return code.DatabaseError
+	}
+	return code.Success
 }
