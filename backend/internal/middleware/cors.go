125a72405a10dd2e5ba70bec9194015f7f972751
LuckyQu <super.lucky.qu@gmail.com>
2025-09-28 11:13:57 +0800

    feat: 处理跨域请求

---
+++ backend/internal/middleware/cors.go
@@ -0,0 +1,28 @@
+// Package middleware cors.go
+//
+// 功能：
+// - 处理跨域请求
+//
+// 作者: LuckyQu
+// 创建日期: 2025-09-25
+// 修改日期: 2025-09-28
+
+package middleware
+
+import (
+	"backend/internal/code"
+	"github.com/gin-gonic/gin"
+)
+
+// CorsMiddleware 处理跨域请求
+func CorsMiddleware(ctx *gin.Context) {
+	ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+	ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+	ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	if ctx.Request.Method == "OPTIONS" {
+		ctx.AbortWithStatus(code.HttpStatusNoContent)
+		return
+	}
+	ctx.Next()
+}
