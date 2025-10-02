4935da1de76f2447fc8350c542f03715df6f1108
LuckyQu <super.lucky.qu@gmail.com>
2025-10-02 19:52:35 +0800

    feat: 新增跨域允许方法

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -5,7 +5,7 @@
 //
 // 作者: LuckyQu
 // 创建日期: 2025-09-25
-// 修改日期: 2025-09-28
+// 修改日期: 2025-10-02
 
 package middleware
 
@@ -18,7 +18,7 @@ import (
 func CorsMiddleware(ctx *gin.Context) {
 	ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 	ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-	ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+	ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	if ctx.Request.Method == "OPTIONS" {
 		ctx.AbortWithStatus(code.HttpStatusNoContent)
