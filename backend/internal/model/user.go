8015bd40e1f5b0c6a417f938c9192c242d34ff6a
LuckyQu <super.lucky.qu@gmail.com>
2025-10-05 02:33:10 +0800

    feat: 用户新增权限属性

--- backend/internal/model/user.go
+++ backend/internal/model/user.go
@@ -5,16 +5,18 @@
 //
 // 作者: LuckyQu
 // 创建日期: 2025-09-24
-// 修改日期: 2025-09-25
+// 修改日期: 2025-10-05
 
 package model
 
 import "gorm.io/gorm"
 
+// User 数据库中用户结构体
 type User struct {
 	gorm.Model
 	Username          string `json:"username"`
 	EncryptedPassword string `json:"encrypted_password"`
+	Role              string `json:"role" `
 	Signature         string `json:"signature"`
 	AvatarPath        string `json:"avatar_path"`
 }
