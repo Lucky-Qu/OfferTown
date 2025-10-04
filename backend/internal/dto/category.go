802bdf8c0db2982a45ab11cee5f52d51b0487127
LuckyQu <super.lucky.qu@gmail.com>
2025-10-05 00:56:31 +0800

    feat: 新增分类接口

---
+++ backend/internal/dto/category.go
@@ -0,0 +1,28 @@
+// Package dto category.go
+//
+// 功能:
+// - 对分类进行增，删，改的DTO模型
+//
+// 作者: LuckyQu
+// 创建日期: 2025-10-05
+// 修改日期: 2025-10-05
+
+package dto
+
+// CreateCategoryDTO 新增分类的DTO模型
+type CreateCategoryDTO struct {
+	Name         string   `json:"name"`
+	QuestionName []string `json:"question_name"`
+}
+
+// UpdateCategoryDTO 修改分类的DTO模型
+type UpdateCategoryDTO struct {
+	Name         string   `json:"name"`
+	QuestionName []string `json:"question_name"`
+	OldName      string   `json:"old_name"`
+}
+
+// DeleteCategoryDTO 删除分类的DTO模型
+type DeleteCategoryDTO struct {
+	Name string `json:"name"`
+}
