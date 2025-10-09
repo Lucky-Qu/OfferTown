829157457133a27ee24c1d068e9f9b449e2e1a24
LuckyQu <super.lucky.qu@gmail.com>
2025-10-09 10:05:33 +0800

    feat: 校验分类合法

---
+++ backend/internal/validator/category.go
@@ -0,0 +1,33 @@
+// Package validator category.go
+//
+// 功能:
+// - 校验当前分类名是否合法
+//
+// 作者: LuckyQu
+// 创建日期: 2025-10-09
+// 修改日期: 2025-10-09
+package validator
+
+import (
+	"backend/internal/code"
+	"backend/internal/repository"
+	"errors"
+	"gorm.io/gorm"
+)
+
+// IsCategoryNameValid 检查分类名是否合法
+func IsCategoryNameValid(categoryName string) (bool, code.Code) {
+	// 长度不为0，不大于32位
+	if l := len(categoryName); l == 0 || l > 32 {
+		return false, code.InvalidCategoryName
+	}
+	// 不能重名
+	_, err := repository.GetCategoryByName(categoryName)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return true, code.Success
+		}
+		return false, code.DatabaseError
+	}
+	return false, code.CategoryNameAlreadyExist
+}
