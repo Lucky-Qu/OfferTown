428b8966b120b09e3a86173df458754b3dcb82df
LuckyQu <super.lucky.qu@gmail.com>
2025-10-10 17:44:52 +0800

    feat: 补全路由注释，对新增的功能进行路由分发

--- backend/internal/router/router.go
+++ backend/internal/router/router.go
@@ -1,5 +1,3 @@
-package router
-
 // Package router router.go
 //
 // 功能:
@@ -8,6 +6,7 @@ package router
 // 作者: LuckyQu
 // 创建日期: 2025-09-24
 // 修改日期: 2025-10-09
+package router
 
 import (
 	"backend/configs"
@@ -25,18 +24,36 @@ func newRouter() *gin.Engine {
 	router := server.Group("/")
 	{
 		router.GET("/ping", api.PingHandler())
-
+		// 基础信息api
+		{
+			// 获取题目列表
+			router.GET("/question", api.GetQuestionListHandler())
+			// 获取分类列表
+			router.GET("/category", api.GetCategoryListHandler())
+			// 获取题目关联的分类或分类关联的题目
+			router.GET("/question-category", api.GetCategoryQuestionHandler())
+			// 获取题目关联的用户
+			router.GET("/question-user", api.GetUsersByQuestion())
+		}
 		// 用户相关api
 		user := router.Group("/user")
 		{
+			// 用户注册
 			user.POST("/register", api.UserRegisterHandler())
+			// 用户登录
 			user.POST("/login", api.UserLoginHandler())
 			// 需要在登陆状态下进行的操作
 			loggedUser := user.Group("/")
 			loggedUser.Use(middleware.JWTAuth)
 			{
+				// 获取当前登录用户信息
 				loggedUser.GET("/info", api.UserInfoHandler())
+				// 更新用户
 				loggedUser.POST("/update", api.UserUpdateHandler())
+				// 获取当前用户做过的题目
+				loggedUser.GET("/user-question", api.GetQuestionsByUser())
+				// 提交题目
+				loggedUser.POST("/submit-answer", api.SubmitAnswerByUser())
 			}
 		}
 		//管理员相关api
@@ -45,11 +62,17 @@ func newRouter() *gin.Engine {
 			admin.Use(middleware.JWTAuth)
 			admin.Use(middleware.AdminPermissionMiddleware)
 			{
+				// 新增题目
 				admin.POST("/add-question", api.AddNewQuestionHandler())
+				// 更新题目
 				admin.POST("/update-question", api.UpdateQuestionHandler())
+				// 删除题目
 				admin.DELETE("/del-question", api.DeleteQuestionHandler())
+				// 添加分类
 				admin.POST("/add-category", api.AddNewCategoryHandler())
+				// 更新分类
 				admin.POST("/update-category", api.UpdateCategoryHandler())
+				// 删除分类
 				admin.DELETE("/del-category", api.DeleteCategoryHandler())
 			}
 		}
