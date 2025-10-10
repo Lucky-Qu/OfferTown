4012b15124408e88658de9a5e213f94942f42817
LuckyQu <super.lucky.qu@gmail.com>
2025-10-10 15:58:35 +0800

    feat: 根据分类或题目获取相关联的题目或分类

---
+++ backend/internal/api/category_question.go
@@ -0,0 +1,46 @@
+// Package api category_question.go
+//
+// 功能:
+// - 获得一道题的所有分类
+// - 获得一个分类的题目数
+// - 获得一个分类的全部题目
+// - 获得一个分类的指定题目
+//
+// 作者: LuckyQu
+// 创建日期: 2025-10-10
+// 修改日期: 2025-10-10
+package api
+
+import (
+	"backend/internal/code"
+	"backend/internal/dto"
+	"backend/internal/service"
+	"github.com/gin-gonic/gin"
+)
+
+// GetCategoriesByQuestionHandler 获取一道题的所有分类或一个分类的指定题目
+func GetCategoriesByQuestionHandler() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		requestDTO := dto.GetCategoryQuestionRequestDTO{}
+		if err := ctx.ShouldBindJSON(&requestDTO); err != nil {
+			ctx.JSON(code.HttpStatusOK, gin.H{
+				"code":    code.BindFailed,
+				"message": code.BindFailed.Msg(),
+			})
+			return
+		}
+		responseDTO, eCode := service.GetCategoryQuestion(&requestDTO)
+		if eCode != code.Success {
+			ctx.JSON(code.HttpStatusOK, gin.H{
+				"code":    eCode,
+				"message": eCode.Msg(),
+			})
+			return
+		}
+		ctx.JSON(code.HttpStatusOK, gin.H{
+			"code":    code.Success,
+			"message": code.Success.Msg(),
+			"data":    responseDTO,
+		})
+	}
+}
